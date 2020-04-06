e676d0b2ef707cd536f70617b8500347803a42a8
Andres <andres@kemeny.cl>
2020-04-06 14:00:08 -0400

    added playground to test before implementation, further improvements to bot - working on bot that answers questions

--- index.js
+++ index.js
@@ -33,33 +33,70 @@ bot = async (hrs) => {
 
     await page.goto(BASE_URL + "/#/program")
 
-    sleep(10000)
+    await page.waitFor(`a[href='/digital1/exercises/exoFrame.jsp?no=1&provenance=33&dest=33&type=worksheet_45&rule=id51233014&&resumeV9=resume']`)
 
     await page.evaluate(() => {
       document.querySelector(`a[href='/digital1/exercises/exoFrame.jsp?no=1&provenance=33&dest=33&type=worksheet_45&rule=id51233014&&resumeV9=resume']`).click();
     });
 
-    sleep(10000)
+    let res00 = "select[name='res[0][0]']";
+    /*
+    let res10 = "select[name='res[1][0]']";
+    let res20 = "select[name='res[2][0]']";
+    let res30 = "select[name='res[3][0]']";
+    let res40 = "select[name='res[4][0]']";
+    */
+
+    sleep(1000)
+
+    await page.waitFor(res00)
+
+    /*
+    let ans = [];
+
+    await page.evaluate((ans) => {
+      let f = []
+      let arr = JSON.parse(document.querySelector("input#correctAnswers").value)
+      console.log(arr.length)
+      for (let i = 0; i < arr.length; i++) {
+        console.log(typeof arr[i], arr[i])
+      }
+      console.log("f", f)
+      console.log(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6])
+    }, ans);
+
+    await page.evaluate((ans) => {
+      console.log(ans)
+    }, ans);
+
+    await page.type(res00, 'shelved');
+    await page.type(res10, 'championed');
+    await page.type(res20, 'plummeted');
+    await page.type(res30, 'patented');
+    await page.type(res40, 'revamped');
+    */
 
     let ms = 3600000 * hrs
 
-    // 540000ms = 9 minutes, assuming inactivity == 10 minutes this should save us
-    // ms/540000 = times loop will run in time given
     let loop = 0
-    let intervalFunc = setInterval(() => {
-      // logic here
-
+    let intervalFunc = setInterval(async () => {
+      await page.click("select[name='res[0][0]']");
+      await page.evaluate((loop) => {
+        console.log("Clicked, loop:", loop);
+      }, loop);
 
       if (++loop === Math.ceil(ms/540000)) {
         clearInterval(intervalFunc)
       }
-    }, 540000)
+    }, 5000)
 
+    await browser.close();
     return `Loop ran ${loop} times.`
   } catch (error) {
     return error
   }
-  
 }
 
+
 bot(4);
+
